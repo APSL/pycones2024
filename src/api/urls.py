972433049e9e6fc032bcba144967b6e1e55f6a40
Nayra Tarazon Barbera <ntarazon@apsl.net>
2024-09-27 19:42:59 +0200

    result: writting your endpoint

--- src/api/urls.py
+++ src/api/urls.py
@@ -16,6 +16,16 @@ urlpatterns = [
     ),
     # clerk / admin
     path(
+        "public/packages/<str:tracking_number>/",
+        AnonPackagesViewSet.as_view({"get": "retrieve"}),
+    ),
+    # courier
+    path(
+        "courier/packages/<str:tracking_number>/tracking/",
+        CourierTrackingViewSet.as_view({"post": "create"}),
+    ),
+    # clerk / admin
+    path(
         "postal-clerk/packages/",
         PostalClerkPackagesViewSet.as_view({"post": "create"}),
     ),
